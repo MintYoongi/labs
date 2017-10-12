27a820758c1b86a6b82bf268c59314e3ab7da053
Kochergina Ekaterina <31899280+MintYoongi@users.noreply.github.com>
2017-10-12 12:17:27 +0300

    Update main.c

--- labs-c/lab3/main.c
+++ labs-c/lab3/main.c
@@ -2,26 +2,34 @@
 #include <math.h>
 
 int main() {
-    int c;
+    int c=1;
     double a, x, G, F, Y;
-    printf ("enter a: ");
-    scanf ("%lf", &a);
-    printf ("enter x: ");
-    scanf ("%lf", &x);
-    for (c=1; c<=3; c++)
-    {   printf ("enter c = 1 or 2 or 3: ");
+    while ((c>=1) && (c<=3)) {
+        printf ("enter c = 1 or 2 or 3: ");
         scanf ("%d", &c);
         switch (c) {
             case 1:
+                printf ("enter a: ");
+                scanf ("%lf", &a);
+                printf ("enter x: ");
+                scanf ("%lf", &x);
                 G = (4 * (-18 * pow(a, 2) + 3 * a * x + 10 * pow(x, 2))) /
                     (15 * pow(a, 2) + 29 * a * x + 12 * pow(x, 2));
                 printf("G = %lf\n\n", G);
                 break;
             case 2:
+                printf ("enter a: ");
+                scanf ("%lf", &a);
+                printf ("enter x: ");
+                scanf ("%lf", &x);
                 F = 1 / (cos(5 * pow(a, 2) + 14 * a * x - 3 * pow(x, 2)));
                 printf("F = %lf\n\n", F);
                 break;
             case 3:
+                printf ("enter a: ");
+                scanf ("%lf", &a);
+                printf ("enter x: ");
+                scanf ("%lf", &x);
                 Y = acos(9 * pow(a, 2) + 42 * a * x + 40 * pow(x, 2) + 1);
                 printf("Y = %lf\n\n", Y);
                 break;
@@ -30,4 +38,3 @@ int main() {
         }
     }
     return 0;
-}
