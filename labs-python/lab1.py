99142756859b64641ba9908c72e8f5c7530d5ffd
Kochergina Ekaterina <31899280+MintYoongi@users.noreply.github.com>
2017-11-09 10:29:37 +0300

    Update lab1.py

--- labs-python/lab1.py
+++ labs-python/lab1.py
@@ -1,16 +1,21 @@
 #import math
 from math import cos, acos
-a = float(input('Enter a: '))
+# ввод значения а
+a = float(input('Enter a: ')) 
+# ввод значения х
 x = float(input('Enter x: '))
+# подсчет G по формуле
 G = (4*(-18*(a**2)+3*a*x+10*(x**2)))/(15*(a**2)+29*a*x+12*(x**2))
-print ('A = {}, X = {}, Result: {}'.format(a,x,G))
+print('A = {}, X = {}, Result: {}'.format(a, x, G))
 
 a = float(input('Enter a: '))
 x = float(input('Enter x: '))
+# подсчет F по формуле
 F = 1/(cos(5*(a**2)+14*a*x-3*(x**2)))
-print ('A = {}, X = {}, Result: {}'.format(a,x,F))
+print('A = {}, X = {}, Result: {}'.format(a, x, F))
 
 a = float(input('Enter a: '))
 x = float(input('Enter x: '))
+# подсчет Y по формуле
 Y = acos(9*(a**2)+42*a*x+40*(x**2)+1)
-print ('A = {}, X = {}, Result: {}'.format(a,x,Y))
+print('A = {}, X = {}, Result: {}'.format(a, x, Y))
