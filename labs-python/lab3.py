bfe8e4127f19dffbbfe8c19554e23c2847c325aa
Kochergina Ekaterina <31899280+MintYoongi@users.noreply.github.com>
2018-01-11 22:19:29 +0400

    Update lab3.py

--- labs-python/lab3.py
+++ labs-python/lab3.py
@@ -1,21 +1,40 @@
-from math import cos, acos
-c = 1
-while (c >= 1) and (c <= 3):
-    c = int(input('Enter c = 1 or 2 or 3: '))
-    if c == 1:
-        a = float(input('Enter a: '))
-        x = float(input('Enter x: '))
-        G = (4*(-18*(a**2)+3*a*x+10*(x**2)))/(15*(a**2)+29*a*x+12*(x**2))
-        print ('a = {}, x = {}, G: {}'.format(a, x, G))
-    if c == 2:
-        a = float(input('Enter a: '))
-        x = float(input('Enter x: '))
-        F = 1/(cos(5*(a**2)+14*a*x-3*(x**2)))
-        print('a = {}, x = {}, F: {}'.format(a, x, F))
-    if c == 3:
-        a = float(input('Enter a: '))
-        x = float(input('Enter x: '))
-        Y = acos(9*(a**2)+42*a*x+40*(x**2)+1)
-        print('a = {}, x = {}, Y: {}'.format(a, x, Y))
-    if (c < 1) or (c > 3):
-        print('Error')
+from math import log, sin, acos
+
+print(" 1 - Вычислить функцию G\n " 
+" 2 - Вычислить функцию F\n " 
+" 3 - Вычислить функцию Y\n " 
+" 0 - Выход из программы \n ") 
+c = float(input('Значение\n')) 
+if c == 0: 
+print("До свидания!") 
+else: 
+a = float(input("Введите а: ")) 
+min = float(input("Введите минимальное значение x: ")) 
+max = float(input("Введите макимальное значение х: ")) 
+step = int(input("Введите число шагов: ")) 
+step = int 
+step = (max - min) / (step - 1) 
+
+if c == 1: 
+while min <= max: 
+m = (15*(a**2)+29*a*x+12*(x**2)) 
+if m != 0: 
+G = (4*(-18*(a**2)+3*a*x+10*(x**2))) / m 
+print('Result: {0:.4f}'.format(G)) 
+else: 
+print("Введите корректное значение ") 
+min += step 
+elif c == 2: 
+while min <= max: 
+m =(cos(5*(a**2)+14*a*x-3*(x**2))) 
+if m != 0: 
+F = 1 / m 
+print('Result: {0:.18f}'.format(F)) 
+else: 
+print("Введите корректное значение") 
+min += step 
+elif c == 3: 
+while min <= max: 
+Y = acos(9*(a**2)+42*a*x+40*(x**2)+1) 
+print('Result: {0:.4f}'.format(Y)) 
+min += step
